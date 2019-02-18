289413c1b3c0e2e66c35f55eb76e09429f2c90c9
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-18 17:35:59 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,11 +1,7 @@
-h
-
 pages << {
         page_type: 'part_number_search',
         method: 'GET',
         url: "https://www.mercadopublico.cl/Portal/Modules/Site/Busquedas/ResultadoBusqueda.aspx?qs=3"
-
-
     }
 
 
